fefb3e87d951bd200beafc6768674df664167871
M0stafa123 <jodat.ahmed135@gmail.com>
2023-11-22 04:30:51 +0200

    upload

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let generate = document.querySelector(".dice");
+let adviceId = document.querySelector(".id");
+let advice = document.querySelector(".advice");
+generate.addEventListener("click", function () {
+    fetch("https://api.adviceslip.com/advice")
+        .then((res) => res.json())
+        .then((data) => {
+        adviceId.innerHTML = data.slip.id;
+        advice.innerHTML = data.slip.advice;
+        console.log(data.slip);
+    });
+});
