1beb4a78b098c0d15ff8a6ee1e7be7fc93b31b22
M0stafa123 <jodat.ahmed135@gmail.com>
2023-11-22 04:43:17 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,11 @@ let generate = document.querySelector(".dice");
 let adviceId = document.querySelector(".id");
 let advice = document.querySelector(".advice");
 generate.addEventListener("click", function () {
-    fetch("https://api.adviceslip.com/advice")
+    fetch("https://api.adviceslip.com/advice?" + new Date().getTime())
         .then((res) => res.json())
         .then((data) => {
         adviceId.innerHTML = data.slip.id;
         advice.innerHTML = data.slip.advice;
+        console.log(data.slip);
     });
 });
